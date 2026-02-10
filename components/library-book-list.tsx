dbd5153f2191aef5e077e339a4a69e47afb128a3
Dani-Carmi <danielecarminati.dc@gmail.com>
2026-02-10 09:23:46 +0100

    fix: update useEffect dependency to ensure books are reloaded correctly

--- components/library-book-list.tsx
+++ components/library-book-list.tsx
@@ -29,7 +29,7 @@ export default function LibraryBookList() {
   useEffect(() => {
     loadBooks();
     //eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [books]);
 
   if (isLoading) {
     return (
